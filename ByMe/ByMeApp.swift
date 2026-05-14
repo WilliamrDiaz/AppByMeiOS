8e8f6a0961c1133af0c2a4a1119b2c9681c1872d
WilliamrDiaz <wrdiaz@unicauca.edu.co>
2026-05-14 09:34:24 -0500

    Initial Commit

---
+++ ByMe/ByMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ByMeApp.swift
+//  ByMe
+//
+//  Created by william diaz on 14/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ByMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ByMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ByMe
+//
+//  Created by william diaz on 14/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
